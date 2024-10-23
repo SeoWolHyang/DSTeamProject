b5133de14334a92f7ec4bdc3262ec72f8d90f11a
SeoWolHyang <hardtack902@gmail.com>
2024-10-23 15:50:38 +0900

    검색 기능 + itemRepository 최적화

--- src/main/java/com/SecondHand/item/ItemController.java
+++ src/main/java/com/SecondHand/item/ItemController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,8 +24,7 @@ public class ItemController {
     @GetMapping("/list")
     String categoryList(Model m,
                         @RequestParam(required = false, defaultValue = "") String category,
-                        @RequestParam(defaultValue = "1") Integer page
-    ){
+                        @RequestParam(defaultValue = "1") Integer page){
         Page<Item> list;
 
         if(category.isEmpty()){ // 정해진 카테고리가 없으면 전체 목록을 반환
@@ -42,11 +42,22 @@ public class ItemController {
     }
 
     // 검색
-    @PostMapping("/search")
-    String postSearch(String searchText, Model m){
-        var result = itemRepository.searchByTitle(searchText);
+    @GetMapping("/list/{searchText}")
+    String searchList(Model m,
+                      @PathVariable String searchText,
+                      @RequestParam(defaultValue = "1") Integer page){
+
+        Page<Item> list = itemRepository.findPageByTitleContains(searchText, PageRequest.of(page -1, 3, Sort.by(Sort.Direction.DESC, "id")));
+
+        if(list.isEmpty())
+            m.addAttribute("warningMessage", "검색 결과가 없습니다.");
+
         m.addAttribute("searchText", searchText);
-        m.addAttribute("items", result);
+        m.addAttribute("items", list.getContent());
+        m.addAttribute("hasPrevious", list.hasPrevious());
+        m.addAttribute("hasNext", list.hasNext());
+        m.addAttribute("currentPage", page);
+        m.addAttribute("totalPage", list.getTotalPages());
 
         return "";
     }
