3ccf42402a9f3cdb91e5e12d53d3e6ce70ed4d2d
SeoWolHyang <hardtack902@gmail.com>
2024-10-24 09:34:52 +0900

    페이징 처리 수정

--- src/main/java/com/SecondHand/item/ItemController.java
+++ src/main/java/com/SecondHand/item/ItemController.java
@@ -71,4 +71,8 @@ public class ItemController {
     public String searchListGet(Model m, @PathVariable String searchText, @RequestParam(defaultValue = "1") Integer page) {
         return processSearchList(m, searchText, page);
     }
+
+    public String test(){
+        return null;
+    }
 }
